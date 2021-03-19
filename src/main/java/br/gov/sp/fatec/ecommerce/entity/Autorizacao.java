5751b00c2338973c2dc5caad8fda0f3729cf7149
Érica Santos <erica.santos2110@gmail.com>
2021-03-19 23:44:36 +0000

    criação das entidades/clases usadas no banco

---
+++ src/main/java/br/gov/sp/fatec/ecommerce/entity/Autorizacao.java
@@ -0,0 +1,56 @@
+package br.gov.sp.fatec.ecommerce.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aut_autorizacao")
+public class Autorizacao {
+
+    // cria o id da tabela autenticacao
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aut_id")
+    private Long id;
+
+    // cria a coluna nome autenticacao
+    @Column(name = "aut_nome")
+    private String nome;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "autorizacoes")
+    private Set<Cliente> clientes;
+   
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public Set<Cliente> getClientes(){
+        return this.clientes;
+    }
+
+    public void setClientes(Set<Cliente> clientes){
+        this.clientes = clientes;
+    }
+    
+}
---
+++ src/main/java/br/gov/sp/fatec/ecommerce/entity/Pedido.java
@@ -0,0 +1,52 @@
+package br.gov.sp.fatec.ecommerce.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+    
+    //cria o id da tabela pedido chave unica
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ped_id")
+    private Long id;
+
+    //cria a coluna valor do pedido
+    @Column(name = "ped_valor")
+    private Float valor;
+
+    //cria o id do cliente no pedido
+    @Column(name = "cli_id")
+    private Long clienteId;
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Float getValor(){
+        return this.valor;
+    }
+
+    public void setValor(Float valor){
+        this.valor = valor;
+    }
+
+    public Long getClienteId(){
+        return this.clienteId;
+    }
+
+    public void setClienteId(Long clienteId){
+        this.clienteId = clienteId;
+    }
+    
+}
