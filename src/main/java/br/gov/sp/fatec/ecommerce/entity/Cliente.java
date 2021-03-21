fabed920653328f4aa9292390a2976f62f45809d
Érica Santos <erica.santos2110@gmail.com>
2021-03-21 22:13:55 +0000

    arrumando a tabela cliente

--- src/main/java/br/gov/sp/fatec/ecommerce/entity/Cliente.java
+++ src/main/java/br/gov/sp/fatec/ecommerce/entity/Cliente.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "cliente")
+@Table(name = "cli_cliente")
 public class Cliente {
 
     //gera o id da tabela cliente
